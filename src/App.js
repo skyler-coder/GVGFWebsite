b02a9d93f99337940a74b81135a2b8a65ba01003
Praise <97951498+praisedisu@users.noreply.github.com>
2024-08-22 14:12:09 +0100

    Update App.js
    new update

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
   return (
     <div className='w-screen h-auto'>
       <Routes>
+        <Route path="/" element={<Preloader />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/gallery" element={<Gallery />} />
         {imageIds.map((link, index) => (
